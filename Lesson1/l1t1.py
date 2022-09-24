9f7032eea87d323fad5ffab50d37fee3c1514a0a
Andrey Sokolov <sokolqe@gmail.com>
2022-09-24 17:40:06 +0300

    Added first three tasks from Lesson1 Workshop

---
+++ Lesson1/l1t1.py
@@ -0,0 +1,9 @@
+print('Enter values:')
+print('a = ')
+a = int(input());
+print('b = ')
+b = int(input());
+if a ** 2 == b or b ** 2 == a:
+    print('Yes')
+else:
+    print('No')
---
+++ Lesson1/l1t3.py
@@ -0,0 +1,6 @@
+print('Enter value of N:')
+n = int(input())
+for i in range(-n, n):
+    print(f'{i}', end=', ')
+print(i+1)
+
