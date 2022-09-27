7a9df9c2852171644b0bd20cd31737d5114b54db
Andrey Sokolov <sokolqe@gmail.com>
2022-09-27 14:51:24 +0300

    Changed path to files

--- Lesson1/l1t2.py
+++
@@ -1,6 +0,0 @@
-max_val = 0
-for i in range(5):
-    print(f"Enter {i+1} value")
-    val = int(input())
-    if val > max_val: max_val = val
-print(f"Maximum value is {max_val}")
--- Lesson1/l1t3.py
+++
@@ -1,6 +0,0 @@
-print('Enter value of N:')
-n = int(input())
-for i in range(-n, n):
-    print(f'{i}', end=', ')
-print(i+1)
-
