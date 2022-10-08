1974c199d11b11c8cce2ad1779aae7337edf1053
Andrey Sokolov <sokolqe@gmail.com>
2022-10-08 17:09:22 +0300

    edit: added code for the first to tasks of homework

--- Lesson4/HomeWork/l4hwt1.py
+++ Lesson4/HomeWork/l4hwt1.py
@@ -4,3 +4,9 @@
 # Enter the required accuracy '0.0001': 0.000001
 # out
 # 9.000000
+
+from decimal import Decimal
+
+number = input('Enter a real number: ')
+acc = input("Enter the required accuracy '0.0001': ")
+print(Decimal(number).quantize(Decimal(acc)))
--- Lesson4/HomeWork/l4hwt2.py
+++ Lesson4/HomeWork/l4hwt2.py
@@ -3,3 +3,36 @@
 # 54
 # out
 # [2, 3, 3, 3]
+
+from math import sqrt
+
+
+def simple_numbers(n):
+    simples = [2]
+    for i in range(3, n + 1, 2):
+        for j in simples:
+            if j * j - 1 > i:
+                simples.append(i)
+                break
+            if (i % j == 0):
+                break
+        else:
+            simples.append(i)
+    return simples
+
+
+def simple_factors(n, simples):
+    factors = []
+    i = 0
+    while n != 1:
+        if n % simples[i] == 0:
+            n //= simples[i]
+            factors.append(simples[i])
+        else:
+            i += 1
+    return factors
+
+
+num = int(input('Enter natural number N: '))
+simp_nums = simple_numbers(num)
+print(simple_factors(num, simp_nums))
