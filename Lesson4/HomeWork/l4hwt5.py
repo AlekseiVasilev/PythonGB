7c7a561521efd7e79cc67d9b7cb863d1d95d0189
Andrey Sokolov <sokolqe@gmail.com>
2022-10-09 10:01:04 +0300

    fix: fixed t5
    finded error. didnt catch (\n)

--- Lesson4/HomeWork/l4hwt5.py
+++ Lesson4/HomeWork/l4hwt5.py
@@ -8,13 +8,19 @@
 # 3*x^9 + 3*x^8 - 2*x^6 + 1*x^5 - 3*x^4 - 3*x^2 + 3 + 2*x^2 + 2*x^1 + 2 = 0
 # 4*x^5 + 1*x^4 - 3*x^3 - 3 + 3*x^3 - 4*x^2 - 2*x^1 - 4 = 0
 # 4*x^2 - 4 + 3*x^6 - 4*x^5 - 4*x^4 - 4*x^3 + 3*x^2 - 2*x^1 - 0 = 0
+from random import choice
+
+
 def addition_from_file(file1, file2):
     with open(file1, 'r', encoding="UTF-8") as output1:
         with open(file2, 'r', encoding="UTF-8") as output2:
             if sum(1 for _ in open(file1, 'r')) == sum(1 for _ in open(file2, 'r')):
-                with open("file1.txt", 'w', encoding="UTF-8") as output:
-                    for i in range(0, sum(1 for _ in output2)):
-                        output.write(output1.readline().strip('=0') + output2.readline())
+                with open("file1.txt", 'a', encoding="UTF-8") as output:
+                    data1 = output1.readlines()
+                    data2 = output2.readlines()
+                    for i in range(len(data1)):
+                        data1[i] = data1[i].strip('0\n').replace('=', choice(['+', '-'])) + data2[i]
+                        output.write(data1[i])
             else:
                 return "Data in files don't match!"
 
