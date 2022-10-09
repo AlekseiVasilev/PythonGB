78ef440c1c431fc3962c9e98f9cbb4d9580cddc5
Andrey Sokolov <sokolqe@gmail.com>
2022-10-09 09:39:00 +0300

    fix: fixed t5 and t5 for homework
    Clarification - Maria Andreeva

--- Lesson4/HomeWork/l4hwt2.py
+++ Lesson4/HomeWork/l4hwt2.py
@@ -7,32 +7,17 @@
 from math import sqrt
 
 
-def simple_numbers(n):
-    simples = [2]
-    for i in range(3, n + 1, 2):
-        for j in simples:
-            if j * j - 1 > i:
-                simples.append(i)
-                break
-            if (i % j == 0):
-                break
-        else:
-            simples.append(i)
-    return simples
-
-
-def simple_factors(n, simples):
+def simple_factors(n):
     factors = []
-    i = 0
+    i = 2
     while n != 1:
-        if n % simples[i] == 0:
-            n //= simples[i]
-            factors.append(simples[i])
+        if n % i == 0:
+            n //= i
+            factors.append(i)
         else:
             i += 1
     return factors
 
 
 num = int(input('Enter natural number N: '))
-simp_nums = simple_numbers(num)
-print(simple_factors(num, simp_nums))
+print(simple_factors(num))
--- Lesson4/HomeWork/l4hwt5.py
+++ Lesson4/HomeWork/l4hwt5.py
@@ -8,25 +8,17 @@
 # 3*x^9 + 3*x^8 - 2*x^6 + 1*x^5 - 3*x^4 - 3*x^2 + 3 + 2*x^2 + 2*x^1 + 2 = 0
 # 4*x^5 + 1*x^4 - 3*x^3 - 3 + 3*x^3 - 4*x^2 - 2*x^1 - 4 = 0
 # 4*x^2 - 4 + 3*x^6 - 4*x^5 - 4*x^4 - 4*x^3 + 3*x^2 - 2*x^1 - 0 = 0
-def compare_files_by_rows(file1, file2):
-    return sum(1 for line in open(file1, 'r')) == sum(1 for line in open(file2, 'r'))
-
-
 def addition_from_file(file1, file2):
-    new_data = []
     with open(file1, 'r', encoding="UTF-8") as output1:
         with open(file2, 'r', encoding="UTF-8") as output2:
-            for i in range(1, sum(1 for line in output2)):
-                new_line = output1.readline(i).strip('=0') + output2.readline(i)
-                new_data.append(new_line)
-    with open(file1, 'w', encoding="UTF-8") as output:
-        for i in range(len(new_data)):
-            output.write(f'{new_data[i]}\n')
-    return
+            if sum(1 for _ in open(file1, 'r')) == sum(1 for _ in open(file2, 'r')):
+                with open("file1.txt", 'w', encoding="UTF-8") as output:
+                    for i in range(0, sum(1 for _ in output2)):
+                        output.write(output1.readline().strip('=0') + output2.readline())
+            else:
+                return "Data in files don't match!"
+
 
 first_file = input('Enter name of the first file: ')
 second_file = input('Enter name of the second file: ')
-if compare_files_by_rows(first_file, second_file):
-    addition_from_file(first_file, second_file)
-else:
-    print("Data in files don't match!")
+addition_from_file(first_file, second_file)
