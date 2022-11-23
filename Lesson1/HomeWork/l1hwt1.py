bb1ce0194edfe4c8c80775a9fdc888cdde125d9b
Andrei <sokolqe@gmail.com>
2022-11-23 09:58:52 +0300

    fix: catching incorrect values

--- Lesson1/HomeWork/l1hwt1.py
+++ Lesson1/HomeWork/l1hwt1.py
@@ -5,11 +5,22 @@
 # - 7 -> да
 # - 1 -> нет
 
-day_number = int(input('Enter day number: '))
-if day_number <= 0 or day_number > 7:
-    print(f'{day_number} is not a day of a week!')
-elif day_number in range(6,8):
+day_number = ""
+while not day_number.isdigit():
+    day_number = input('Enter day number: ')
+    if day_number.isdigit():
+        day_number = int(day_number)
+        if day_number < 1 or day_number > 7:
+            day_number = ""
+            print('Value is incorrect\nIf you want to exit the program then type "exit"')
+        else:
+            break
+    elif day_number == "exit":
+        exit('Bye-bye')
+    else:
+        print('Value is incorrect\nIf you want to exit the program then type "exit"')
+
+if day_number in range(6, 8):
     print("It's a weekend, let's party!")
 else:
     print("Boooriiiing, it's not a weekend")
-
