3dbcba3eb47b108151c2e931651d0291264fc1ff
Andrey Sokolov <sokolqe@gmail.com>
2022-11-26 19:24:57 +0300

    ver: task4 ver 1.0 while cycle seems better

--- Lesson2/HomeWork/l2hwt4.py
+++ Lesson2/HomeWork/l2hwt4.py
@@ -14,41 +14,65 @@ def input_method():
         players_number = int(input('Type value for players number: '))
         if players_number > 0:
             players_list = [[i, 0] for i in range(1, players_number + 1)]
-            return players_list
+            return players_list, 1
         else:
-            return exit('Value is incorrect')
+            return exit('Very funny. Only positive number')
     except ValueError:
-        return exit('Value is incorrect')
+        return exit('Rly...how we start without people?!')
 
-def game_algo(round_result, round_amount):
-    master_count = randint(0, len(round_result) - 1)
-    print(f'One-two...i choose you #{round_result[master_count][0]}! WHOAAAHHAHAH')
-    for game_breakpoint in range(master_count + 1):
-        coins_amount = 1
-        round_result[game_breakpoint][1] += coins_amount
-    for game_breakpoint in range(master_count + 1, len(round_result)):
-        coins_amount = 2
-        round_result[game_breakpoint][1] += coins_amount
-    print(f'Results after {round_amount} round:')
-    for i in round_result:
-        print(f'Player #{i[0]} have coins = {i[1]}')
+
+def master_choose():
     try:
-        round_result[master_count + 1][1] += round_result[master_count][1]
-    except IndexError:
-        round_result[0][1] += round_result[master_count][1]
-    print(f'\n------------------\nPlayer #{round_result[master_count][0]} is out!\n------------------\n')
-    round_result.pop(master_count)
+        while True:
+            mode = int(input('Do you want to be a Game Master, or destiny will choose? (1/2):'))
+            if mode == -1:
+                exit('So...bye-bye')
+            elif mode == 1 or mode == 2:
+                return mode
+            else:
+                print('Choose 1 or 2, or type "-1" to end the process.')
+    except ValueError:
+        exit('No-no-no...1 or 2 next time')
 
-    for i in round_result:
-        print(f'Player #{i[0]} have coins = {i[1]}')
-    return round_result
 
-a = input_method()
+def game_algo(round_result, round_amount, start_position, game_mode):
+    print('Start =', start_position)
+    if game_mode == 1:
+        try:
+            master_count = int(input('Hmmm...how many steps do you want?\n'))
+            if master_count <= 0:
+                exit('How you wanna count with this value?! Type positive number next time!')
+        except ValueError:
+            exit('Facepalm...')
+    else:
+        master_count = randint(1, len(round_result))
+    hash = []
+    while master_count != 0:
+        if start_position < len(round_result):
+            round_result[start_position-1][1] += 1
+            hash.append(start_position)
+        else:
+            start_position = 1
+            round_result[start_position-1][1] += 1
+            hash.append(start_position)
+        start_position += 1
+        master_count -= 1
+    for i in range(len(round_result)):
+        if round_result[i][0] not in hash:
+            round_result[i][1] += 2
+    print(round_result)
+    print(start_position)
+    round_result.pop(start_position-2)
+    print(round_result)
+    return round_result, start_position - 1
+
+
+a, start = input_method()
+game_mode = master_choose()
 for i in range(len(a) - 1):
-    a = game_algo(a, i+1)
+    a, start = game_algo(a, i + 1, start, game_mode)
     if len(a) > 1:
         print('\nNext round start in 3 seconds!\n------------------')
         sleep(3)
     else:
         exit(f'Our winner is Player #{a[0][0]}.\nYour jackpot is {a[0][1]}.000.000 $\nCongratulations!!!')
-
