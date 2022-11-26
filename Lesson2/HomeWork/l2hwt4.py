91f6c4457a75103f6f675f4460b3ff90cbf63921
Andrey Sokolov <sokolqe@gmail.com>
2022-11-26 21:12:39 +0300

    ver: task4 ver 2.0 done

--- Lesson2/HomeWork/l2hwt4.py
+++ Lesson2/HomeWork/l2hwt4.py
@@ -16,63 +16,70 @@ def input_method():
             players_list = [[i, 0] for i in range(1, players_number + 1)]
             return players_list, 1
         else:
-            return exit('Very funny. Only positive number')
+            exit('Very funny. Only positive number')
     except ValueError:
-        return exit('Rly...how we start without people?!')
+        exit('Rly...how we start without people?!')
 
 
 def master_choose():
     try:
-        while True:
-            mode = int(input('Do you want to be a Game Master, or destiny will choose? (1/2):'))
-            if mode == -1:
-                exit('So...bye-bye')
-            elif mode == 1 or mode == 2:
-                return mode
-            else:
-                print('Choose 1 or 2, or type "-1" to end the process.')
+        mode = int(input('Do you want to be a Game Master, or destiny will choose? (1/2):'))
+        if mode == 1 or mode == 2:
+            return mode
+        else:
+            exit("Choose 1 or 2...it's not so hard, isn't it?")
     except ValueError:
         exit('No-no-no...1 or 2 next time')
 
 
 def game_algo(round_result, round_amount, start_position, game_mode):
-    print('Start =', start_position)
     if game_mode == 1:
         try:
             master_count = int(input('Hmmm...how many steps do you want?\n'))
             if master_count <= 0:
                 exit('How you wanna count with this value?! Type positive number next time!')
+            if master_count > len(round_result):
+                print("Uhhh...i'm tired, you can't count more then pepople's amount, so")
+                master_count = len(round_result)
         except ValueError:
             exit('Facepalm...')
     else:
         master_count = randint(1, len(round_result))
     hash = []
     while master_count != 0:
-        if start_position < len(round_result):
-            round_result[start_position-1][1] += 1
-            hash.append(start_position)
+        if start_position <= len(round_result):
+            round_result[start_position - 1][1] += 1
+            hash.append(round_result[start_position - 1][0])
         else:
             start_position = 1
-            round_result[start_position-1][1] += 1
-            hash.append(start_position)
+            round_result[start_position - 1][1] += 1
+            hash.append(round_result[start_position - 1][0])
         start_position += 1
         master_count -= 1
     for i in range(len(round_result)):
         if round_result[i][0] not in hash:
             round_result[i][1] += 2
-    print(round_result)
-    print(start_position)
-    round_result.pop(start_position-2)
-    print(round_result)
+            hash.append(i)
+    print(f"Let's count the coins after {round_amount} round before elimination...")
+    for i in range(len(round_result)):
+        print(f'Player #{round_result[i][0]}: {round_result[i][1]} coins')
+    try:
+        round_result[start_position - 1][1] += round_result[start_position - 2][1]
+    except IndexError:
+        round_result[0][1] += round_result[start_position - 2][1]
+    print(f'\n------------\nWhoaahah...PLAYER #{round_result[start_position - 2][0]}! I CHOOSE YOU!\n------------\n')
+    round_result.pop(start_position - 2)
+    print(f"Let's count the coins after elimination...")
+    for i in range(len(round_result)):
+        print(f'Player #{round_result[i][0]}: {round_result[i][1]} coins')
     return round_result, start_position - 1
 
 
-a, start = input_method()
+game_data, start = input_method()
 game_mode = master_choose()
-for i in range(len(a) - 1):
-    a, start = game_algo(a, i + 1, start, game_mode)
+for i in range(len(game_data) - 1):
+    a, start = game_algo(game_data, i + 1, start, game_mode)
     if len(a) > 1:
         print('\nNext round start in 3 seconds!\n------------------')
         sleep(3)
-    else:
-        exit(f'Our winner is Player #{a[0][0]}.\nYour jackpot is {a[0][1]}.000.000 $\nCongratulations!!!')
+print(f'Our winner is Player #{game_data[0][0]}.\nYour jackpot is {game_data[0][1]}.000.000 $\nCongratulations!!!')
