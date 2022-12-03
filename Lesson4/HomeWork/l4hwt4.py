89ba4f0624db9ad3e399f1e2c679e0ab2ce75bb5
Andrey Sokolov <sokolqe@gmail.com>
2022-12-03 20:57:40 +0300

    ver: l4hwt4 ver. 1.1 cath enc-dec functions logic errors

--- Lesson4/HomeWork/l4hwt4.py
+++ Lesson4/HomeWork/l4hwt4.py
@@ -5,11 +5,12 @@
 # а также функцию, которая спрашивает ключ, считывает текст и дешифровывает его.
 
 from string import *
+from os import path
 
 
 def file_create() -> None:
     '''
-    Creates .txt file with text encrypted by caesar encription key.
+    Creates caesar_enc.txt file
 
     :return: info about operation
     '''
@@ -21,17 +22,22 @@ def file_create() -> None:
 
 
 def enc_key_caesar() -> int:
-    enc_key = []
     while True:
+        enc_key = []
         key = input('Type key for caesar encryption.\n'
                     'For ex.: 2L - 2 symbols left, 3R - 3 symbols right\n'
                     '>> ').lower()
-        if key[0].isdigit() and key[1].isdigit():
-            enc_key.append(key[0] + key[1])
-            enc_key.append(key[2])
-        else:
-            enc_key = list(key)
-        if len(enc_key) > 2 or not enc_key[0].isdigit() or not enc_key[1] in ('r', 'l'):
+        enc_key.append(key[0])
+        for i in range(1, len(key)):
+            if key[i].isdigit():
+                enc_key[0] += key[i]
+            else:
+                enc_key.append(key[i])
+        try:
+            if len(enc_key) > 2 or not enc_key[0].isdigit() or not enc_key[1] in ('r', 'l'):
+                print('Key contains only natural number (from 1 to 25) and one letter (r, l). Try again!')
+                continue
+        except IndexError:
             print('Key contains only natural number (from 1 to 25) and one letter (r, l). Try again!')
             continue
         else:
@@ -54,25 +60,32 @@ def enc_func_caesar(key: int) -> None:
             if line[i] in ascii_lowercase:
                 symbol_index = ascii_lowercase.find(line[i])
                 if symbol_index >= 0:
-                    symbol_index = symbol_index + key
+                    symbol_index = symbol_index - key
+                    if symbol_index >= len(ascii_lowercase) or symbol_index < len(ascii_lowercase) * (-1):
+                        symbol_index = symbol_index % len(ascii_lowercase)
                     line[i] = ascii_lowercase[symbol_index]
             elif line[i] in ascii_uppercase:
                 symbol_index = ascii_uppercase.find(line[i])
                 if symbol_index >= 0:
-                    symbol_index = symbol_index + key
+                    symbol_index = symbol_index - key
+                    if symbol_index >= len(ascii_uppercase) or symbol_index < len(ascii_uppercase) * (-1):
+                        symbol_index = symbol_index % len(ascii_uppercase)
                     line[i] = ascii_uppercase[symbol_index]
             elif line[i] in digits:
                 symbol_index = digits.find(line[i])
                 if symbol_index >= 0:
-                    symbol_index = symbol_index + key
+                    symbol_index = symbol_index - key
+                    if symbol_index >= len(digits) or symbol_index < len(digits) * (-1):
+                        symbol_index = symbol_index % len(digits)
                     line[i] = digits[symbol_index]
             if line[i] in punctuation:
                 symbol_index = punctuation.find(line[i])
                 if symbol_index >= 0:
-                    symbol_index = symbol_index + key
+                    symbol_index = symbol_index - key
+                    if symbol_index >= len(punctuation) or symbol_index < len(punctuation) * (-1):
+                        symbol_index = symbol_index % len(punctuation)
                     line[i] = punctuation[symbol_index]
         enc_data = ''.join(line)
-        print(enc_data)
     with open('caesar_enc.txt', 'w', encoding='UTF8') as file:
         file.write(enc_data)
     return print('Success!\nInformation was encrypted in caesar_enc.txt')
@@ -86,30 +99,77 @@ def dec_func_caesar(key: int) -> None:
                 symbol_index = ascii_lowercase.find(line[i])
                 if symbol_index >= 0:
                     symbol_index = symbol_index - key
+                    if symbol_index >= len(ascii_lowercase) or symbol_index < len(ascii_lowercase) * (-1):
+                        symbol_index = symbol_index % len(ascii_lowercase)
                     line[i] = ascii_lowercase[symbol_index]
             elif line[i] in ascii_uppercase:
                 symbol_index = ascii_uppercase.find(line[i])
                 if symbol_index >= 0:
                     symbol_index = symbol_index - key
+                    if symbol_index >= len(ascii_uppercase) or symbol_index < len(ascii_uppercase) * (-1):
+                        symbol_index = symbol_index % len(ascii_uppercase)
                     line[i] = ascii_uppercase[symbol_index]
             elif line[i] in digits:
                 symbol_index = digits.find(line[i])
                 if symbol_index >= 0:
                     symbol_index = symbol_index - key
+                    if symbol_index >= len(digits) or symbol_index < len(digits) * (-1):
+                        symbol_index = symbol_index % len(digits)
                     line[i] = digits[symbol_index]
             if line[i] in punctuation:
                 symbol_index = punctuation.find(line[i])
                 if symbol_index >= 0:
                     symbol_index = symbol_index - key
+                    if symbol_index >= len(punctuation) or symbol_index < len(punctuation) * (-1):
+                        symbol_index = symbol_index % len(punctuation)
                     line[i] = punctuation[symbol_index]
         enc_data = ''.join(line)
-        print(enc_data)
     with open('caesar_enc.txt', 'w', encoding='UTF8') as file:
         file.write(enc_data)
     return print('Success!\nInformation was deciphered in caesar_enc.txt')
 
 
-file_create()
-key_enc = enc_key_caesar()
-enc_func_caesar(key=key_enc)
-dec_func_caesar(key=key_enc)
+def menu_input():
+    '''
+    Creates console menu for user
+
+    :return:
+    '''
+
+    while True:
+        print('Type "1" if you want to create file')
+        print('Type "2" if you want to create encrypted file')
+        print('Type "3" if you want to decipher existing file')
+        print('Type "-1" if you want to exit')
+        num = input('What operation do you want to do?: ')
+        if num == '1':
+            file_create()
+        elif num == '2':
+            if path.exists(fr'.\caesar_enc.txt'):
+                caesar_key = enc_key_caesar()
+                enc_func_caesar(key=caesar_key)
+            else:
+                print('File caesar_enc.txt is not exist in this folder.')
+                print("Would you like to create file? (Y/N)")
+                if input().lower() in ('y', 'ye', 'yes'):
+                    file_create()
+                else:
+                    print("Create file caesar_enc.txt and put it to root folder\n")
+        elif num == '3':
+            if path.exists(fr'.\caesar_enc.txt'):
+                caesar_key = enc_key_caesar()
+                dec_func_caesar(key=caesar_key)
+            else:
+                print('File caesar_enc.txt is not exist in this folder.')
+                print("Would you like to create file? (Y/N)")
+                if input().lower() in ('y', 'ye', 'yes'):
+                    file_create()
+                else:
+                    print("Create file caesar_enc.txt and put it to root folder\n")
+        elif num == '-1':
+            return exit(print("Bye-bye"))
+        else:
+            print("Incorrect value, try again!")
+
+
+menu_input()
