a5d1e51ce54ebe2a591ae52c270689ceba6fcab9
Andrey Sokolov <sokolqe@gmail.com>
2022-12-03 17:07:47 +0300

    add: l4hwt3 release

---
+++ Lesson4/HomeWork/l4hwt3.py
@@ -0,0 +1,59 @@
+# 3 - В файле, содержащем фамилии студентов и их оценки,
+# изменить на прописные буквы фамилии тех студентов, которые имеют средний балл более «4».
+# Нужно перезаписать файл.
+
+from gbfunctions import give_int
+
+
+def create_file(iter: int) -> str:
+    '''
+    Creates .txt file with lines filled by student's names and grades
+
+    :param iter: amount of students
+    :return: name of the file
+    '''
+
+    with open('students_and_grades.txt', 'w', encoding='UTF8') as file:
+        for _ in range(iter):
+            name = input("Type student's name: ")
+            file.write(f'{name} ')
+            while True:
+                grade = input(f"Type grade for {name}: ")
+                if grade.isdigit() and 0 < int(grade) <= 5:
+                    file.write(f'{grade}\n')
+                    break
+                else:
+                    print('Type integer number from 1 to 5, please')
+                    continue
+    return 'students_and_grades.txt'
+
+
+def edit_file(file_name: str, grade: int) -> str:
+    '''
+    Edit student names from "file_name" file to upper case if their grade higher
+    then "grade" parameter.
+
+    :param file_name:
+    :param grade:
+    :return: name of edited file
+    '''
+
+    with open(file_name, 'r', encoding='UTF8') as file:
+        lines_data = []
+        for line in file.readlines():
+            data_string = line.strip('\n')
+            student_grade = int(data_string[-1])
+            if student_grade >= grade:
+                lines_data.append(data_string.upper() + '\n')
+            else:
+                lines_data.append(data_string + '\n')
+    with open(file_name, 'w', encoding='UTF8') as file:
+        for i in lines_data:
+            file.write(i)
+    return file_name
+
+
+amount = give_int('Type amount of students: ', 1)
+file_name = create_file(amount)
+result_file = edit_file(file_name, 4)
+print(f'You can see your work in {result_file}')
